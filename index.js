5c2c899e6022f531a649b8622611d4a19438c98d
meperdonass <135055452+meperdonass@users.noreply.github.com>
2023-05-30 15:35:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function() {
-    alert('Gracias <3, escribime jaja')
+    window.location.href = "perro.html";
 });
 
 const noBtn = document.querySelector('#noBtn');
